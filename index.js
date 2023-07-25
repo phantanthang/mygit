d7f460227a4cc30f216574971990e5bfb5c325dc
phantanthang <thangphan@kms-technology.com>
2023-07-25 11:47:26 +0700

    Add new api for about page

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/", () => {
   console.log("Hello from homepage");
 });
 
+app.get("/about", () => {
+  console.log("Hello from about page");
+});
+
 app.listen(PORT, () =>
   console.log(`Server is listenning on the port: ${PORT}`)
 );
